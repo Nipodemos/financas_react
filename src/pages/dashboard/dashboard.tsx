d0f1d37ebbf148940696365155f48dfa1e720e8c
Nipodemos <nipodemos13@gmail.com>
2022-07-02 18:16:56 -0300

    adicionado começo da tela de dashboard

---
+++ src/pages/dashboard/dashboard.tsx
@@ -0,0 +1,33 @@
+export const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <button>Adicionar Despesa</button>
+      <button>Adcionar Receita</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Valor</th>
+            <th>Data</th>
+            <th>Pago</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Água</td>
+            <td>R$50,00</td>
+            <td>22/02/2022</td>
+            <td>🆗</td>
+            <td>
+              <button>Definir como pago</button>
+              <button>Editar</button>
+              <button>Excluir</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
